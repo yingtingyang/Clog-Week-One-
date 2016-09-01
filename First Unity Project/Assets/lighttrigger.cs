4b3f19e9fd8213c939965df2aa565be7d0ad27b9
YINGTINGYANG <yangy389@newschool.edu>
2016-09-01 18:29:31 -0400

    initial unity commit

---
+++ First Unity Project/Assets/lighttrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lighttrigger : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // when something enters the Trigger attached to this game object
+    void OnTriggerEnter()
+    {
+        // get the light attached to this object
+        // turn that light on
+        GetComponent<Light>().enabled = true;
+        GetComponent<Light>().color = Color.cyan;
+    }
+    void OnTriggerExit()
+    {
+        //turn the light off when you leave
+        GetComponent<Light>().enabled = true;
+        GetComponent<Light>().color = Color.red;
+    }
+}
+
